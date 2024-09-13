45fe76a6338d4e27d89e7db5e3b05ba0f1a0e9f2
Linda <lzheng50@stuy.edu>
2024-09-13 11:01:38 -0400

    working on random dictionary

---
+++ 04_rando-dict/krewes.py
@@ -0,0 +1,12 @@
+"""
+Linda Zheng
+Boas
+SoftDev
+K<04> -- <Random Number Generation>
+2024-09-13
+time spent: 0.2
+"""
+
+import random
+krewes = {3:['Linda', 'Michelle','Ryan'], 42:['Linda', 'Michelle', 'Ryan'], 888:['Linda', 'Michelle', 'Ryan']}
+print(random.choice(random.choice(krewes)))
