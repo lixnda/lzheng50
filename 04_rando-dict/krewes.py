977da20efbadeb6e351428d6d19e0380a0e09593
lixnda <lzheng50@stuy.edu>
2024-09-15 23:52:12 -0400

    finished random generator

--- 04_rando-dict/krewes.py
+++ 04_rando-dict/krewes.py
@@ -8,5 +8,20 @@ time spent: 0.2
 """
 
 import random
-krewes = {3:['Linda'], 42:['Michelle'], 888:['Ryan']}
-print()
+krewes = {
+           4: [
+        'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+        'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+        'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+        'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+        ],
+        5: [ 
+            'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+            'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+            'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+            'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN'
+        ]
+    }
+
+#random choice only works with list, hence why the dict was converted
+print(random.choice(list(random.choice(list(krewes.values())))))
