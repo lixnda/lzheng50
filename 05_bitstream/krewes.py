492a38e90b87dfa0fcd51327f558c0d32edee1eb
Linda <lzheng50@stuy.edu>
2024-09-18 11:01:52 -0400

    fixed issues

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -6,7 +6,7 @@ K<05> -- Bitstream
 2024-09-17
 time spent: 0.5
 
-Issues: would not work if names had space in them due to string parcings
+Issues: would not work if names had space in them due to string parcings (FIXED)
 """
 
 import random
@@ -14,12 +14,24 @@ import random
 file = open("krewes.txt", "r")
 krewes = {}
 
-data = file.read().replace("@", " ").replace("$", " ").strip()
-table = data.split()
+data=file.read()
 
+data = data.replace("@", "$")
+outputdata = ""
+for i in range(len(data) -1):
+    if not(i != 0 and data[i] == "$" and data[i - 1] == "$"):
+        outputdata = outputdata + data[i]
+
+print (outputdata)
+
+data = outputdata
+table = data.split("$")
+#print(table)
 keyVal = 0
 
-i = 0
+
+#brute force to remove index 0
+i = 1
 while i<(len(table)-1):
     if(table[i].isnumeric()):
         keyVal = int(table[i])
