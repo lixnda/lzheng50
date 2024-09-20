96a6ad4e531b83235c7e6d95b9d1349a562f6d21
lixnda <lzheng50@stuy.edu>
2024-09-19 22:51:50 -0400

    finished wkshop 6

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -16,16 +16,26 @@ data = file.read().strip().split("\n")
 
 
 occupation = {}
+weight = []
 
 for i in range(1, len(data)-1):
     job = data[i]
     if job[0]=='"':
         key = job[1:].index('"')
+        
+        #replacing necessary comma with placeholder
         temp = job[0:key].replace(",","<>") + job[key:]
         data[i] = temp
 
 for i in range(1, len(data)-1):
     splitted = data[i].split(",")
-    print (splitted)
-    splitted[0].replace("<>", ",")
+    
+    #replacing placeholder with comma
+    splitted[0] = splitted[0].replace("<>", ",")
     occupation[splitted[0]] = splitted[1]
+    weight.append(float(splitted[1]))
+
+ 
+randO = random.choices(list(occupation), weights=weight, k=500)
+
+print(randO)
